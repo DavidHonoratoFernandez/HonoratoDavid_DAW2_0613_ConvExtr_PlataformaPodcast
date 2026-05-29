3f2baf4d72816845d6c06b7d2a1e6c5059383463
DavidHonoratoFernandez <463507.joan23@fje.edu>
2026-05-29 21:52:49 +0200

    Arreglos en el apartado de conexion

--- config/conexion.php
+++ config/conexion.php
@@ -1,19 +1,21 @@
 <?php
+class Database {
+    private $srv_name = "localhost";
+    private $db_username = "root";
+    private $db_passwd = "";
+    private $db_name = "db_podcast";
+    public $conn;
 
-$srv_name = "localhost";
-$db_username = "root";
-$db_passwd = "";
-$db_name = "db_podcast";
-
-
-try {
-
-    $conn = new PDO("mysql:host=$srv_name;dbname=$db_name", $db_username, $db_passwd);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-} catch (PDOException $e){
-
-    echo "Error en la conexión con el server de datos: " . $e->getMessage();
-    die();
-
+    public function getConnection() {
+        $this->conn = null;
+        try {
+            $this->conn = new PDO("mysql:host=" . $this->srv_name . ";dbname=" . $this->db_name, $this->db_username, $this->db_passwd);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e){
+            echo "Error en la conexión con el server de datos: " . $e->getMessage();
+            die();
+        }
+        return $this->conn;
+    }
 }
+?>
